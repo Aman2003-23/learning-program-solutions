58a607ae59e61da52f114d6a952d9ff22974ba89
Aman Agarwal <121810126+Aman2003-23@users.noreply.github.com>
2025-07-11 13:30:05 +0530

    week4_solution3

--- SpringLearnApplication.java
+++ SpringLearnApplication.java
@@ -5,23 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-	public static void displayDate() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 
-		try {
-			Date date = format.parse("31/12/2018");
-			System.out.println("Parsed Date: " + date);
-		} catch (ParseException e) {
-			System.out.println("Date parsing failed: " + e.getMessage());
-		}
-	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringLearnApplication.class, args);
 		System.out.println("running spring app..");
-		displayDate();
+
 	}
 
 }
