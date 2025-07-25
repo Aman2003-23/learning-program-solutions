ed470a5aecd8445013de9f6049c004ebaac716e0
Aman Agarwal <121810126+Aman2003-23@users.noreply.github.com>
2025-07-25 14:35:58 +0530

    Week6_solutions

---
+++ About.js
@@ -0,0 +1,12 @@
+
+import React,{Component} from 'react';
+
+class About extends Component {
+    render() {
+        <div>
+        <h4>Welcome to the About Page of Student Management Portal</h4>
+        </div>
+    }
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+ class Contact extends Component {
+    render() {
+        return (
+        <div>
+        <h4>Welcome to the Contact Page of student Management Portal</h4>
+        </div>
+        );
+    }
+}
+
+export default Contact;
